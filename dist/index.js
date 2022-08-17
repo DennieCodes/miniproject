b31084e7aea418af601042a4c82deac368e301dc
Oculareo <oculareo@gmail.com>
2022-08-17 08:53:27 -0400

    Added form structure for project

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,22 @@
 "use strict";
-function printDouble(msg) {
-    console.log(msg, msg);
-}
-console.log(printDouble('Project is set-up.  '));
+const button = document.getElementById('btn');
+const input = document.getElementById('todoinput');
+const list = document.getElementById('todolist');
+const form = document.querySelector('form'); // Typescript knows that form is an HTML element
+// also
+// const form = document.querySelector('#todoform')! as HTMLFormElement;
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const newTodoText = input.value;
+    const newLI = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    newLI.append(newTodoText);
+    newLI.append(checkbox);
+    list.append(newLI);
+    input.value = '';
+});
+// also.  This function is not in the context of HTML form element so you have to annotate type for e which you don't have to above.
+// function handleSubmit(e: SubmitEvent) {
+//   e.preventDefault();
+// }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,26 @@
-function printDouble(msg: string) {
-	console.log(msg, msg);
-}
+const button = document.getElementById('btn')! as HTMLButtonElement;
+const input = document.getElementById('todoinput')! as HTMLInputElement;
+const list = document.getElementById('todolist')! as HTMLUListElement;
 
-console.log(printDouble('Project is set-up.  '));
+const form = document.querySelector('form')!; // Typescript knows that form is an HTML element
+// also
+// const form = document.querySelector('#todoform')! as HTMLFormElement;
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	const newTodoText = input.value;
+	const newLI = document.createElement('li');
+	const checkbox = document.createElement('input');
+	checkbox.type = 'checkbox';
+	newLI.append(newTodoText);
+	newLI.append(checkbox);
+	list.append(newLI);
+
+	input.value = '';
+});
+
+// also.  This function is not in the context of HTML form element so you have to annotate type for e which you don't have to above.
+// function handleSubmit(e: SubmitEvent) {
+//   e.preventDefault();
+// }
