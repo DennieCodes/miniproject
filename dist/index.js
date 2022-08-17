61fa90b0b65b5d6aeadce3103d653d0a82bfb0df
Oculareo <oculareo@gmail.com>
2022-08-16 21:26:55 -0400

    Basic Project setup

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function printDouble(msg) {
+    console.log(msg, msg);
+}
+console.log(printDouble('Project is set-up.  '));
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function printDouble(msg: string) {
+	console.log(msg, msg);
+}
+
+console.log(printDouble('Project is set-up.  '));
