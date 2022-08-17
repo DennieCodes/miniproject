7e93a7e2e31e6124d9e973e9ad6e1999b5d63de0
Oculareo <oculareo@gmail.com>
2022-08-17 09:17:37 -0400

    Added local storage, toggle checkbox

--- dist/index.js
+++ dist/index.js
@@ -2,21 +2,46 @@
 const button = document.getElementById('btn');
 const input = document.getElementById('todoinput');
 const list = document.getElementById('todolist');
-const form = document.querySelector('form'); // Typescript knows that form is an HTML element
-// also
-// const form = document.querySelector('#todoform')! as HTMLFormElement;
+const form = document.querySelector('form'); // Typescript knows that form is an HTML element: const form = document.querySelector('#todoform')! as HTMLFormElement;
+const todos = readTodos();
+todos.forEach(createTodo);
+// Read Todos from Local Storage if present
+function readTodos() {
+    const todosJSON = localStorage.getItem('todos');
+    if (todosJSON === null)
+        return [];
+    return JSON.parse(todosJSON);
+}
+function saveTodos() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+// Submit handler for form
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const newTodoText = input.value;
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    todos.push(newTodo);
+    createTodo(newTodo);
+    saveTodos();
+    input.value = '';
+    // also.  This function is not in the context of HTML form element so you have to annotate type for e which you don't have to above.
+    // function handleSubmit(e: SubmitEvent) {
+    //   e.preventDefault();
+    // }
+});
+// Create todo HTML element
+function createTodo(todo) {
     const newLI = document.createElement('li');
     const checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    newLI.append(newTodoText);
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener('change', () => {
+        todo.completed = checkbox.checked;
+        saveTodos();
+    });
+    newLI.append(todo.text);
     newLI.append(checkbox);
     list.append(newLI);
-    input.value = '';
-});
-// also.  This function is not in the context of HTML form element so you have to annotate type for e which you don't have to above.
-// function handleSubmit(e: SubmitEvent) {
-//   e.preventDefault();
-// }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,60 @@
 const button = document.getElementById('btn')! as HTMLButtonElement;
 const input = document.getElementById('todoinput')! as HTMLInputElement;
 const list = document.getElementById('todolist')! as HTMLUListElement;
+const form = document.querySelector('form')!; // Typescript knows that form is an HTML element: const form = document.querySelector('#todoform')! as HTMLFormElement;
 
-const form = document.querySelector('form')!; // Typescript knows that form is an HTML element
-// also
-// const form = document.querySelector('#todoform')! as HTMLFormElement;
+interface Todo {
+	text: string;
+	completed: boolean;
+}
 
+const todos: Todo[] = readTodos();
+
+todos.forEach(createTodo);
+
+// Read Todos from Local Storage if present
+function readTodos(): Todo[] {
+	const todosJSON = localStorage.getItem('todos');
+	if (todosJSON === null) return [];
+	return JSON.parse(todosJSON);
+}
+
+function saveTodos() {
+	localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+// Submit handler for form
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 
-	const newTodoText = input.value;
+	const newTodo: Todo = {
+		text: input.value,
+		completed: false,
+	};
+	todos.push(newTodo);
+	createTodo(newTodo);
+
+	saveTodos();
+	input.value = '';
+	// also.  This function is not in the context of HTML form element so you have to annotate type for e which you don't have to above.
+	// function handleSubmit(e: SubmitEvent) {
+	//   e.preventDefault();
+	// }
+});
+
+// Create todo HTML element
+function createTodo(todo: Todo) {
 	const newLI = document.createElement('li');
 	const checkbox = document.createElement('input');
 	checkbox.type = 'checkbox';
-	newLI.append(newTodoText);
+	checkbox.checked = todo.completed;
+
+	checkbox.addEventListener('change', () => {
+		todo.completed = checkbox.checked;
+		saveTodos();
+	});
+
+	newLI.append(todo.text);
 	newLI.append(checkbox);
 	list.append(newLI);
-
-	input.value = '';
-});
-
-// also.  This function is not in the context of HTML form element so you have to annotate type for e which you don't have to above.
-// function handleSubmit(e: SubmitEvent) {
-//   e.preventDefault();
-// }
+}
